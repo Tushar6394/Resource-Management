872fb3b8f8ffbb23b13ca766da7312208b2636db
Tushar Singh <tusharsingh06.ts@gmail.com>
2025-06-11 23:34:34 +0530

    Add files via upload

---
+++ ResourceManagement-main/drizzle.config.js
@@ -0,0 +1,11 @@
+export default {
+    dialect: "postgresql",
+    schema: "./src/utils/db/schema.ts",
+    out: "./drizzle",
+    dbCredentials: {
+      url: "postgresql://neondb_owner:bIDF7e9wsjnu@ep-square-heart-a1rp4vev.ap-southeast-1.aws.neon.tech/w2w?sslmode=require",
+      connectionString:
+        "postgresql://neondb_owner:bIDF7e9wsjnu@ep-square-heart-a1rp4vev.ap-southeast-1.aws.neon.tech/w2w?sslmode=require",
+    },
+  };
+  
